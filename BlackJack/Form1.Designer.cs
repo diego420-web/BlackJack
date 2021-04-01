8e5439d01b7278e3d44f185ec9402b916c27a9e5
Diego <diego.agent@gmail.com>
2021-04-01 18:24:11 -0300

    BLACKJACK JOGO DE CARTAS

---
+++ BlackJack/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace BlackJack
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_pontos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox1.Image = global::BlackJack.Properties.Resources._0;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // lbl_pontos
+            // 
+            resources.ApplyResources(this.lbl_pontos, "lbl_pontos");
+            this.lbl_pontos.Name = "lbl_pontos";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.lbl_pontos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_pontos;
+    }
+}
+
---
+++ BlackJack/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlackJack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int pontos = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x;
+            Random sorteio = new Random();
+
+            x = sorteio.Next(1, 13);
+
+            switch (x)
+            {
+
+                case 1:
+                    pictureBox1.Image = Properties.Resources.a;
+                    pontos += 1;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources._2;
+                    pontos += 2;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources._3;
+                    pontos += 3;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources._4;
+                    pontos += 4;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources._5;
+                    pontos += 5;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources._6;
+                    pontos += 6;
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources._7;
+                    pontos += 7;
+                    break;
+                case 8:
+                    pictureBox1.Image = Properties.Resources._8;
+                    pontos += 8;
+                    break;
+                case 9:
+                    pictureBox1.Image = Properties.Resources._9;
+                    pontos += 9;
+                    break;
+                case 10:
+                    pictureBox1.Image = Properties.Resources._10;
+                    pontos += 10;
+                    break;
+                case 11:
+                    pictureBox1.Image = Properties.Resources.J;
+                    pontos += 11;
+                    break;
+                case 12:
+                    pictureBox1.Image = Properties.Resources.Q;
+                    pontos += 12;
+                    break;
+                case 13:
+                    pictureBox1.Image = Properties.Resources.K;
+                    pontos += 13;
+                    break;
+            }
+            if (pontos <= 21)
+            {
+                lbl_pontos.Text = Convert.ToString(pontos);
+                if (pontos == 21)
+                {
+
+                    lbl_pontos.Text = "VOCÊ GANHOU !!";
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+
+                }
+            }
+            else
+            {
+                lbl_pontos.Text = "VOCÊ PERDEU !! (" + Convert.ToString(pontos) + "PONTOS)";
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lbl_pontos.Text = "0";
+            pontos = 0;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            pictureBox1.Image = Properties.Resources._0;
+
+        }
+    }
+}
