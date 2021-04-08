5926b2d54d818e4fd23c9474d2285198f3e839d6
Diego <diego.agent@gmail.com>
2021-04-07 22:28:48 -0300

    Agora podemos jogar de dupla

--- BlackJack/Form1.Designer.cs
+++ BlackJack/Form1.Designer.cs
@@ -35,7 +35,14 @@ namespace BlackJack
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.lbl_pontos = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultado = new System.Windows.Forms.Label();
+            this.lbl_result = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -61,6 +68,7 @@ namespace BlackJack
             resources.ApplyResources(this.pictureBox1, "pictureBox1");
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // label1
             // 
@@ -71,12 +79,55 @@ namespace BlackJack
             // 
             resources.ApplyResources(this.lbl_pontos, "lbl_pontos");
             this.lbl_pontos.Name = "lbl_pontos";
+            this.lbl_pontos.Click += new System.EventHandler(this.lbl_pontos_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox2.Image = global::BlackJack.Properties.Resources._0;
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // resultado
+            // 
+            resources.ApplyResources(this.resultado, "resultado");
+            this.resultado.Name = "resultado";
+            // 
+            // lbl_result
+            // 
+            resources.ApplyResources(this.lbl_result, "lbl_result");
+            this.lbl_result.Name = "lbl_result";
             // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.lbl_pontos);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
@@ -89,6 +140,7 @@ namespace BlackJack
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,6 +153,12 @@ namespace BlackJack
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lbl_pontos;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.Label lbl_result;
     }
 }
 
--- BlackJack/Form1.cs
+++ BlackJack/Form1.cs
@@ -16,13 +16,14 @@ namespace BlackJack
         {
             InitializeComponent();
             button2.Enabled = false;
+            button3.Enabled = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-        int pontos = 0;
+        int pontos = 0, pontosy = 0;
         private void button1_Click(object sender, EventArgs e)
         {
             int x;
@@ -35,85 +36,231 @@ namespace BlackJack
 
                 case 1:
                     pictureBox1.Image = Properties.Resources.a;
+                    
                     pontos += 1;
                     break;
                 case 2:
                     pictureBox1.Image = Properties.Resources._2;
+                    
                     pontos += 2;
                     break;
                 case 3:
                     pictureBox1.Image = Properties.Resources._3;
+                    
                     pontos += 3;
                     break;
                 case 4:
                     pictureBox1.Image = Properties.Resources._4;
+                    
                     pontos += 4;
                     break;
                 case 5:
                     pictureBox1.Image = Properties.Resources._5;
+                   
                     pontos += 5;
                     break;
                 case 6:
                     pictureBox1.Image = Properties.Resources._6;
+                    
                     pontos += 6;
                     break;
                 case 7:
                     pictureBox1.Image = Properties.Resources._7;
+                   
                     pontos += 7;
                     break;
                 case 8:
                     pictureBox1.Image = Properties.Resources._8;
+                   
                     pontos += 8;
                     break;
                 case 9:
                     pictureBox1.Image = Properties.Resources._9;
+                   
                     pontos += 9;
                     break;
                 case 10:
                     pictureBox1.Image = Properties.Resources._10;
+                    
                     pontos += 10;
                     break;
                 case 11:
                     pictureBox1.Image = Properties.Resources.J;
+                   
                     pontos += 11;
                     break;
                 case 12:
                     pictureBox1.Image = Properties.Resources.Q;
+                    
                     pontos += 12;
                     break;
                 case 13:
                     pictureBox1.Image = Properties.Resources.K;
-                    pontos += 13;
+                   
                     break;
             }
+            
             if (pontos <= 21)
             {
                 lbl_pontos.Text = Convert.ToString(pontos);
                 if (pontos == 21)
                 {
 
-                    lbl_pontos.Text = "VOCÊ GANHOU !!";
+                    //lbl_result.Text = "JOGADOR 1 GANHOU !";
                     button1.Enabled = false;
-                    button2.Enabled = true;
-
+                    
+                    button3.Enabled = true;
                 }
             }
             else
             {
-                lbl_pontos.Text = "VOCÊ PERDEU !! (" + Convert.ToString(pontos) + "PONTOS)";
+                //lbl_result.Text = "JOGADOR 1 PERDEU ! (" + Convert.ToString(pontos) + ")" ;
                 button1.Enabled = false;
-                button2.Enabled = true;
+                
+                button3.Enabled = true;
             }
             
+        }
+
+        
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int y;
+            Random sorteio = new Random();
+
+            y = sorteio.Next(1, 13);
+
+            switch (y)
+            {
+
+                case 1:
+                   
+                    pictureBox2.Image = Properties.Resources.a;
+                    pontosy += 1;
+                    break;
+                case 2:
+                    
+                    pictureBox2.Image = Properties.Resources._2;
+                    pontosy += 2;
+                    break;
+                case 3:
+                   
+                    pictureBox2.Image = Properties.Resources._3;
+                    pontosy += 3;
+                    break;
+                case 4:
+                   
+                    pictureBox2.Image = Properties.Resources._4;
+                    pontosy += 4;
+                    break;
+                case 5:
+                    
+                    pictureBox2.Image = Properties.Resources._5;
+                    pontosy += 5;
+                    break;
+                case 6:
+                    
+                    pictureBox2.Image = Properties.Resources._6;
+                    pontosy += 6;
+                    break;
+                case 7:
+                   
+                    pictureBox2.Image = Properties.Resources._7;
+                    pontosy += 7;
+                    break;
+                case 8:
+                   
+                    pictureBox2.Image = Properties.Resources._8;
+                    pontosy += 8;
+                    break;
+                case 9:
+                   
+                    pictureBox2.Image = Properties.Resources._9;
+                    pontosy += 9;
+                    break;
+                case 10:
+                   
+                    pictureBox2.Image = Properties.Resources._10;
+                    pontosy += 10;
+                    break;
+                case 11:
+                   
+                    pictureBox2.Image = Properties.Resources.J;
+                    pontosy += 11;
+                    break;
+                case 12:
+                    
+                    pictureBox2.Image = Properties.Resources.Q;
+                    pontosy += 12;
+                    break;
+                case 13:
+                    
+                    pictureBox2.Image = Properties.Resources.K;
+                    break;
+            }
+            
+            if (pontosy <= 21)
+            {
+                label3.Text = Convert.ToString(pontosy);
+                if (pontosy == 21)
+                {
+
+                    //lbl_result.Text = "JOGADOR 2 GANHOU !";
+                    button3.Enabled = false;
+                    button2.Enabled = true;
+                    button1.Enabled = false;
+                    resultados();
+                }
+            }
+            else
+            {
+                //lbl_result.Text = "JOGADOR 2 PERDEU ! (" + Convert.ToString(pontosy) + ")";
+                button3.Enabled = false;
+                button2.Enabled = true;
+                resultados();
+            }
+
+        }
+        public void resultados()
+        {
+            if (pontos == 21)
+                lbl_result.Text = "JOGADOR 1 GANHOU";
+            else
+                if (pontosy == 21)
+                lbl_result.Text = "JOGADOR 2 GANHOU";
+            else
+                if (pontos == 21 && pontosy == 21)
+                lbl_result.Text = "EMPATE";
+            else
+                lbl_result.Text = "SEM VENCEDOR";
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             lbl_pontos.Text = "0";
+            lbl_result.Text = " ";
+            label3.Text = "0";
             pontos = 0;
+            pontosy = 0;
             button1.Enabled = true;
             button2.Enabled = false;
+            button3.Enabled = false;
             pictureBox1.Image = Properties.Resources._0;
+            pictureBox2.Image = Properties.Resources._0;
+
+        }
+
+        private void lbl_pontos_Click(object sender, EventArgs e)
+        {
 
         }
     }
